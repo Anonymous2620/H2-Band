56edc1f0834ef56f00d8fd913068e691dc85ecce
Anonymous2620 <69890106+Anonymous2620@users.noreply.github.com>
2021-11-25 18:00:41 +0100

    Add files via upload

--- H2 Band/band.js
+++ H2 Band/band.js
@@ -1,106 +1,90 @@
 
-
-class PlayAudio {
-    constructor(name, location){
-        this.name = name;
-        this.location = location;
+class Instrument
+{
+    set Name(name){
+        this.iName = name;
+    }
+    get Name(){
+        return this.iName;
+    }
+    Play(sound){
+        var audio = new Audio(sound);
+        audio.play();
     }
 }
 
-let flute = new PlayAudio("flute", "music/flute.mp3") 
-let guitar = new PlayAudio("guitar", "music/guitar.mp3")
-let harp = new PlayAudio("harp", "music/harp.mp3")
-let saxophone = new PlayAudio("saxophone", "music/saxophone.mp3")
+class Stringed extends Instrument
+{
+    set NumberofStrings(amount){
+        this.NumberofStrings = amount;
+    }
 
-var playF = new Audio(flute.location); 
-var playG = new Audio(guitar.location); 
-var playH = new Audio(harp.location); 
-var playS = new Audio(saxophone.location); 
-
-function loopAudio1(){
-    var loop1 = document.getElementById('loop1');
-    if (loop1.checked == true){
-        playF.loop = true;
-    } else {
-        playF.loop = false;
+    get NumberofStrings(){
+        return this.NumberofStrings;
+    }
+    constructor(numberOfStrings){
+        super();
+        this.numberOfStrings = numberOfStrings;
     }
 }
 
-function loopAudio2(){
-    var loop2 = document.getElementById('loop2');
-    if (loop2.checked == true){
-        playG.loop = true;
-    } else {
-        playG.loop = false
+class Harp extends Stringed
+{
+    constructor(height){
+        super(4);
+        this.height = height;
+        super.Name = "Harp";
     }
 }
 
-function loopAudio3(){
-    var loop3 = document.getElementById('loop3');
-    if (loop3.checked == true){
-        playH.loop = true;
-    } else {
-        playH.loop = false
+class Guitar extends Stringed
+{
+    constructor(material){
+        super(4);
+        super.Name = material;  
     }
 }
 
-function loopAudio4(){
-    var loop4 = document.getElementById('loop4');
-    if (loop4.checked == true){
-        playS.loop = true;
-    } else {
-        playS.loop = false
+class Saxophone extends Instrument
+{
+    constructor(material){
+        super(4);
+        super.Name = material;
     }
 }
 
-function sliderChange1(val1) {
-    document.getElementById('sliderVal1').innerHTML = val1;
-    playF.volume = val1;
+class Flute extends Instrument
+{
+    constructor(holes, material){
+        super(4);
+        this.holes = holes;
+        super.Name = material;
+        
+    }
 }
 
-function sliderChange2(val2) {
-    document.getElementById('sliderVal2').innerHTML = val2;
-    playG.volume = val2;
+var harp = new Harp(4);
+var guitar = new Guitar("Guitar");
+var saxo = new Saxophone("Saxophone");
+var flute = new Flute("Flute");
+
+function OnClick()
+{
+    var selected = document.getElementById("instrumentMenu").value;
+    switch (selected){
+        case "Guitar":
+            guitar.Play("music/guitar.mp3");    
+            break;
+        case "Harp":
+            harp.Play("music/harp.mp3")
+            break;
+        case "Saxophone":
+            saxo.Play("music/saxophone.mp3");
+            break;
+        case "Flute":
+            flute.Play("music/flute.mp3");
+            break;
+        default:
+            break;
+    }
 }
-
-function sliderChange3(val3) {
-    document.getElementById('sliderVal3').innerHTML = val3;
-    playH.volume = val3;
-}
-
-function sliderChange4(val4) {
-    document.getElementById('sliderVal4').innerHTML = val4;
-    playS.volume = val4;
-}
-
-
-function playFlute(){
-    playF.play();
-}
-
-function playGuitar(){
-    playG.play();
-}
-
-function playHarp(){
-    playH.play();
-}
-
-function playSaxophone(){
-    playS.play();
-}
-
-function playAll(){
-    playF.play();
-    playG.play();
-    playH.play();
-    playS.play();
-}
-
-//audio.oncanplaythrough = function(){
- // audio.play();
-//}
-
-//audio.onended = function(){
-   
-//}
